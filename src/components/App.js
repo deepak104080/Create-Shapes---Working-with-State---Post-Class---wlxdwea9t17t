cbe556c1c614ad56629eb42b00039e322167c3af
deepak104080 <deepak104080@gmail.com>
2023-02-11 23:35:47 +0530

    Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+
+  const [selectedVal, setSelectedVal] = useState('square'); //to temporarily store dropdown value
+  const [shapeArray, setShapeArray] = useState([]); //array to iterate for displaying circle or square
+
+  const selectFn = (e) => {
+    console.log('select done.', e.target.value);
+    setSelectedVal(e.target.value);
+  }
+
+  const selectedFn = () => {
+    console.log('selected fn - latest value - ', selectedVal);
+    setShapeArray([...shapeArray, selectedVal]);
+  }
 
   return (
     <div id="main">
       <div id="shape-creator">
-        
+        <select onChange={selectFn}>
+          <option value="square">Square</option>
+          <option value="circle">Circle</option>
+        </select>
+
+        <button onClick={selectedFn}>Add Shape</button>
 
       </div>
       <div id="shapes-holder">
+        {shapeArray.map((item, index) => (
+          <div className={item}>{index}</div>
+        ))}
       </div>
     </div>
   )
